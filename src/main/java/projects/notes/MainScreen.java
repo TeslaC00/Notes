dc11eceeaffc5332669f5ab7bde77b1f5011856e
TeslaC00 <vikaskumar16122002@gmail.com>
2023-12-14 23:06:32 +0530

    choice box to combobox

--- src/main/java/projects/notes/MainScreen.java
+++ src/main/java/projects/notes/MainScreen.java
@@ -3,7 +3,7 @@ package projects.notes;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -43,7 +43,6 @@ public class MainScreen implements Initializable {
             note.setTitle(titleTextField.getText());
             note.setContent(mainTextArea.getText());
         }
-
         System.err.println("Auto saved");
     }
 
